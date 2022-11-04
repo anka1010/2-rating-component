c09cdf733a9bd37dca5725923107a8953adab863
anka1010 <an.louc@gmail.com>
2022-11-04 12:39:23 +0300

    Refactor markup, style and logic for rating buttons

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-/* Clicked button stays activated */
-const rateBtns = document.querySelectorAll('.btn');
 
-rateBtns.forEach((button) => {
-  button.addEventListener('click', () => {
-    rateBtns.forEach((rateBtn) => {
-      return rateBtn.classList.remove('btn-selected');
-    });
-    button.classList.add('btn-selected');
-  });
-});
